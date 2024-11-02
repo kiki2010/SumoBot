677aeb15cde5a5fec1ce0c9f3dcc21993e504cae
kiki2010 <chiaraangelinacatalini@gmail.com>
2024-11-02 16:48:19 -0300

    cambios agregados

--- SumoBot Robot/src/main.cpp
+++ SumoBot Robot/src/main.cpp
@@ -1,2 +1,31 @@
 #include <Arduino.h>
 #include <VirtualWire.h>
+
+const int dataPin = 9;
+const int ledPin = 12;
+
+void setup(){
+    vw_setup(2000);
+    vw_set_rx_pin(dataPin);
+    vw_rx_start();
+}
+
+void loop(){
+    uint8_t data;
+    uint8_t dataLenght = 1;
+
+    if (vw_get_message(&data,&dataLenght))
+    {
+        if ((char)data =='a')
+        {
+            digitalWrite(ledPin, true);
+        }
+        else if ((char)data=='b')
+        {
+            digitalWrite(ledPin, false);
+        }
+        
+        
+    }
+    
+}
--- SumoControl/src/main.cpp
+++ SumoControl/src/main.cpp
@@ -1,1 +1,20 @@
 #include <Arduino.h>
+#include <VirtualWire.h>
+
+const int dataPin = 9;
+
+void setup(){
+    Serial.begin(9600);
+    vw_setup(2000);
+    vw_set_tx_pin(dataPin);
+}
+
+void loop(){
+    while (Serial.avaible() > 0){
+        char data[1];
+        data[0] = Serial.read();
+        vw_send((uiny8_t*)data,sizeof(data));
+        vw_wait_tx();
+    }
+    delay(200);
+}
